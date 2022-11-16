c8a3df26cbbdd1d91352cf6c80a41eb0885754db
Hannah414 <113860275+Hannah414@users.noreply.github.com>
2022-11-16 13:57:24 -0500

    Add files via upload

---
+++ Program_1.c
@@ -0,0 +1,16 @@
+//here we include out header file that runs inputs and outputs 
+#include <stdio.h>
+
+//here we name and open a funtion in order to print "Hello World"
+int main() {
+
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+
+   // return is used to send data out from inside a function
+   // there's nothing here the needs to be sent out, so we return 0
+   // we will talk more about return late in the tutorial
+   return 0;
+
+// close the function
+}
