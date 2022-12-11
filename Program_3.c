222df64bc59fb0dfeca609d9b97aa7fdb984ef6b
Hannah414 <113860275+Hannah414@users.noreply.github.com>
2022-12-10 20:35:13 -0500

    Add files via upload

---
+++ Program_3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// this program will address operators in c
+int main() {
+
+// addition, subtraction, multiplication, and division
+// addition and subtraction use + and - as expected
+// multiplication uses * and division uses / or %
+int a = 5 + 5; //10
+int b = 5 - 5; //0
+int c = 5 * 2; //10
+int d = 10 / 2; //5
+
+// <, >, <=, >= are used for less then, greater then
+// less then or equal to, and greater then or equal to respectivly
+
+//for use in conditional statments
+// || and | is used for 'either or' or both
+// ^ is used for 'either or'
+// & and && mean 'and', though & is also used for pointers
+
+
+return 0;
+}
